6b8ad43f81b67f5e18ad75b642a73212c366cdbb
unknown <f219477@NUCFD.EDU.LOCAL>
2023-10-06 10:21:11 +0500

    Enhanced the functionality of caculator project by adding main class

---
+++ F219651_F219477_JavaProject/src/calculatorpkg/Main.java
@@ -0,0 +1,13 @@
+package calculatorpkg;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Calcluator cal = new Calcluator();
+		System.out.print("Adding 2 numbers "+ cal.add(2, 200000));
+
+		System.out.print("\nsubstracting 2 numbers "+cal.sub(2, 200000));
+		
+	}
+
+}
